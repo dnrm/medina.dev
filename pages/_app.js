59f170d9adc887f6a8e27abe91c1833a631efae1
Daniel Medina <daniel@medina.com>
2021-08-18 10:32:33 -0500

    :sparkles: Created custom _document.js to load font

--- pages/_app.js
+++ pages/_app.js
@@ -5,18 +5,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 function MyApp({ Component, pageProps }) {
     return (
         <ChakraProvider>
-            <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossOrigin="true"
-                />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap"
-                    rel="stylesheet"
-                />
-            </Head>
             <Component {...pageProps} />
         </ChakraProvider>
     );
