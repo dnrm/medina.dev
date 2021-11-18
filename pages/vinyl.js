80e2b5f733cb002d6d14f334c7eb12c83cc5f152
Daniel Medina <daniel@medina.com>
2021-11-17 22:12:18 -0600

    :sparkles: Added information about vinyl records and added christmas lights

--- pages/vinyl.js
+++ pages/vinyl.js
@@ -20,53 +20,59 @@ const padding = 6;
 
 const collection = [
   {
-    title: "Definitely Maybe",
+    title: "Definitely Maybe (Remastered)",
     artist: "Oasis",
     cover: "https://i.scdn.co/image/ab67616d0000b273d86a1e021e7acc7c07c7d668",
     link: "https://open.spotify.com/album/50xG9YujTzMGaLHfJTskBy?si=05PZTYgtRY2npzMCBXKepA"
   },
   {
-    title: "Be Here Now",
+    title: "Be Here Now (Remastered)",
     artist: "Oasis",
     cover: "https://i.scdn.co/image/ab67616d0000b273aeda362a434f01d0eff70b4e",
     link: "https://open.spotify.com/album/5WsCU95SaumguegxnC87TV?si=roj9rrKaTHOy4tgMe_ttgA"
   },
   {
-    title: "A Rush of Blood to the Head",
+    title: "A Rush of Blood to the Head 1xLP",
     artist: "Coldplay",
     cover: "https://i.scdn.co/image/ab67616d0000b27390afd8e4ec6d787114ed6c40",
     link: "https://open.spotify.com/album/0RHX9XECH8IVI3LNgWDpmQ?si=rNn19WQ7QC-rVcLKLvzLpQ"
   },
   {
-    title: "A Head Full of Dreams",
+    title: "A Head Full of Dreams 2xLP Black",
     artist: "Coldplay",
     cover: "https://i.scdn.co/image/ab67616d0000b2738ff7c3580d429c8212b9a3b6",
     link: "https://open.spotify.com/album/3cfAM8b8KqJRoIzt3zLKqw?si=494e3052096c4594"
   },
   {
-    title: "Different Class",
+    title: "Different Class 1xLP",
     artist: "Pulp",
     cover: "https://i.scdn.co/image/ab67616d0000b273fd952bece8f049dbcd7df93f",
     link: "https://open.spotify.com/album/3ly9T2L4pqTZijFgQssd3x?si=D-OEbo_eQQegg3loECpEcg"
   },
   {
-    title: "(What's The Story) Morning Glory?",
+    title: "(What's The Story) Morning Glory? 2xLP",
     artist: "Oasis",
     cover: "https://i.scdn.co/image/ab67616d0000b2737a4c8c59851c88f6794c3cbf",
     link: "https://open.spotify.com/album/1VW1MFNstaJuygaoTPkdCk?si=IsrKBGb9QrCohwV8Irn45A"
   },
   {
-    title: "Happier Than Ever",
+    title: "Happier Than Ever 2xLP, Black",
     artist: "Billie Eilish",
     cover: "https://i.scdn.co/image/ab67616d0000b2732a038d3bf875d23e4aeaa84e",
     link: "https://open.spotify.com/album/0JGOiO34nwfUdDrD612dOp?si=UGHhEai3QAatUqFEobVm9Q"
   },
   {
-    title: "Double Fantasy (Original 1980 Pressing)",
+    title: "Double Fantasy (Original 1980 Mexican Pressing)",
     artist: "John Lennon & Yoko Ono",
     cover: "https://i.scdn.co/image/ab67616d0000b2730488a5c4e21edc24526652ae",
     link: "https://open.spotify.com/album/1NWA2fPLUAW5df7UGI5thp?si=20d588db7c97488b"
   },
+  {
+    title: "Christmas Lights Blue 7\" Single 2020",
+    artist: "Coldplay",
+    cover: "https://i.scdn.co/image/ab67616d0000b2734b3f858e8ace8ca943695ace",
+    link: "https://open.spotify.com/track/4MJmHcZn440Wf7oIeMbhD1"
+  }
 ];
 
 const Vinyl = () => {
