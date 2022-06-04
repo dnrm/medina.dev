7d9cfc2880d817af1365ba008cbad26703665bbe
Daniel Medina <daniel@medina.com>
2022-06-04 12:23:48 -0500

    :chart_with_upwards_trend: Added Google Analytics tags

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
 import theme from "../theme";
+import Script from "next/script";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -23,6 +24,20 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-QTFWKHLPEV"
+          ></script>
+          <Script
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-QTFWKHLPEV');`,
+            }}
+          ></Script>
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
